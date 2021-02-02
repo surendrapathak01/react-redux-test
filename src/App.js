c01c5056e411496d9cef5b1a7a71b30f493339f1
Surendra <surendra.pathak@cloudyfox.com>
2021-02-02 14:57:16 +0545

    added node sass and other assets

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import React from 'react';
+import Header from './components/header';
 
 function App() {
  
   return (
     <div className="App">
-      <h1>Counter:</h1>
+      <Header />
     </div>
   );
 }
